78acaf15a4eabcc0a25c8f6db3f56ea3523fd158
Renato Müller Rosa <renatogordooo@gmail.com>
2022-04-06 12:27:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# OLXBikeScrapper
+ Projeto criado com o intuito de estudar um pouco mais sobre web scraping, node e facilitar minha vida buscando motos na OLX
---
+++ src/scraping.js
@@ -0,0 +1,58 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const extractAllData = async (targetUrl) => {
+    try {
+        const response = await axios.get(targetUrl)
+        return response.data
+    } catch (error) {
+        console.log("Can't extract data from this targetUrl" + targetUrl)
+    }
+}
+
+const listAllLinks = async (targetUrl) => {
+    const linksExtracted = []
+    const html = await extractAllData(targetUrl);
+    const $ = await cheerio.load(html);
+    $('.sc-12rk7z2-1').each(function(i, lnk) {
+        linksExtracted[i] = $(lnk).attr('href')
+    })
+    return linksExtracted
+}
+
+const extractRelevantData = async (productLink) => {
+    try {
+        const response = await axios.get(productLink)
+        const htmlAnuncio = response.data
+        const $ = await cheerio.load(htmlAnuncio)
+        let nomeProduto = $('#content > div.sc-18p038x-3.dSrKbb > div > div.sc-bwzfXH.h3us20-0.cBfPri > div.duvuxf-0.h3us20-0.jAHFXn > div.h3us20-6.gFNxVM > div > div > h1').text()
+        let valor = $('#content > div.sc-18p038x-3.dSrKbb > div > div.sc-bwzfXH.h3us20-0.cBfPri > div.duvuxf-0.h3us20-0.cpscHx > div.h3us20-6.jUPCvE > div > div > div.sc-hmzhuo.dtdGqP.sc-jTzLTM.iwtnNi > div.sc-hmzhuo.sc-12l420o-0.kUWFYY.sc-jTzLTM.iwtnNi > h2.sc-ifAKCX.eQLrcK').text()
+        let dataPublicacao = $('#content > div.sc-18p038x-3.dSrKbb > div > div.sc-bwzfXH.h3us20-0.cBfPri > div.duvuxf-0.h3us20-0.jAHFXn > div.h3us20-6.hzUJDA > div > div > div > span.sc-1oq8jzc-0.jvuXUB.sc-ifAKCX.fizSrB').text()
+        const resultado = {
+            nomeProduto: nomeProduto,
+            valor: valor,
+            dataPublicacao: dataPublicacao,
+            link: productLink
+        }
+        return resultado
+    } catch (error) {
+        console.log('Houve um problema na extração dos dados' + error)
+    }
+}
+
+const getDataFromUrl = async (targetUrl) => {
+    try {
+        var data = {} // empty Object
+        var key = targetUrl;
+        data[key] = [];
+        const linksExtracted = await listAllLinks(targetUrl)
+        for (const productLink of linksExtracted) {
+            const extractedData = await extractRelevantData(productLink)
+            data[key].push(extractedData)
+        };
+        console.log(data)
+    } catch (error) {
+        console.log('Ops! something went wrong' + error)
+    }
+}
